8cd0fc9ee03bf791490642d39578ee0aa7678eaf
davekempsell <dave_kempsell@hotmail.com>
2022-12-09 17:14:38 +0000

    react app fetches activities list from API on load. Total distance and remaining distance displayed

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,40 @@ import React, { useState, useEffect } from 'react'
 const App = () =>{
 
   const [data, setData] = useState(null)
+  const [total, setTotal] = useState(0)
+
+  function totalMeters(data) {
+    let total = 0
+    data.forEach(element => {
+      total += element.distance
+    })
+    return total / 1000
+  }
 
   useEffect(() => {
     fetch('http://localhost:5050/')
       .then((res) => res.json())
-      .then((data) => setData(data))
-  })
+      .then((data) => {
+        setData(data)
+        setTotal(totalMeters(data))
+      })
+      .catch((error) => {
+        console.log(error)
+        setData('error loading data')
+      })
+  },[])
+
+
 
   return (
-    <h1>
-      {!data ? 'Loading... ' : data}
-    </h1>
+    <div>
+      <h1>
+        Total Distance: {total.toFixed(2)}km
+      </h1>
+      <h2>
+        Distance remaining: {500 - total.toFixed(2)}km
+      </h2>
+    </div>
   )
 }
 
