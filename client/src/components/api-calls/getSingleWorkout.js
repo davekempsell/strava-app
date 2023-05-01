b3628ea66721b55227254cf5a1695e3ce70e878b
Davekempsell <dave.kempsell@marshmallow.co>
2023-05-01 16:31:06 +0100

    fetch address changed to render address

--- client/src/components/api-calls/getSingleWorkout.js
+++ client/src/components/api-calls/getSingleWorkout.js
@@ -1,6 +1,6 @@
 export function getSingleWorkout(workoutId) {
-  // fetch('https://strava-api-server.onrender.com')
-  return fetch(`http://localhost:5050/${workoutId}`)
+  return fetch(`https://strava-api-server.onrender.com/${workoutId}`)
+  // return fetch(`http://localhost:5050/${workoutId}`)
     .then((res) => res.json())
     .then((data) => {
       return data
