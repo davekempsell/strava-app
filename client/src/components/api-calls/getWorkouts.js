b8259e5aa855be38c4aa10aeceb26b1b06e8d779
Davekempsell <dave.kempsell@marshmallow.co>
2023-05-22 12:49:19 +0100

    api call updated

--- client/src/components/api-calls/getWorkouts.js
+++ client/src/components/api-calls/getWorkouts.js
@@ -1,7 +1,8 @@
 export function getWorkouts() {
-  const apiUrl = process.env.REACT_APP_API_URL || 'https://strava-api-server.onrender.com';
+  // const apiUrl = process.env.REACT_APP_API_URL || 'https://strava-api-server.onrender.com';
 
-  return fetch(apiUrl + '/workouts/get-workouts')
+  // return fetch(apiUrl + '/workouts/get-workouts')
+  return fetch('https://strava-api-server.onrender.com/workouts/get-workouts')
     .then((res) => res.json())
     .then((data) => { return data})
     .catch((error) => {
